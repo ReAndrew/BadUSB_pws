dd851971c043d5dc762618f17a354923b3de780b
ReAndrew <reandrew24@gmail.com>
2026-02-14 22:05:41 +0500

    Create redirect.ps1

---
+++ redirect.ps1
@@ -0,0 +1,20 @@
+
+@{
+    # "Команда" = "Имя_файла_на_гитхабе"
+    "physics"  = "WindowDance-physics.ps1"
+    "circle"   = "WindowDance-circle.ps1"
+    "sinus"    = "WindowDance-sinus.ps1"
+    "caps"     = "WindowDance-caps.ps1"
+    "chaos"    = "WindowDance-chaos.ps1"
+    "debug"    = "Debug.ps1"
+    "manager"  = "Menu.ps1"
+    "download" = "Downloader.ps1"
+    
+    # Алиасы (синонимы)
+    "local"    = "Downloader.ps1"
+    "menu"     = "Menu.ps1"
+    "shake"    = "WindowDance-chaos.ps1"
+    "jump"     = "WindowDance-sinus.ps1"
+    "dance"    = "WindowDance-circle.ps1"
+    "stop"     = "kill"
+}
