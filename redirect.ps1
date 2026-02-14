d01d3877d59d57fab8e83d8861f4582c3748c077
ReAndrew <reandrew24@gmail.com>
2026-02-14 22:07:58 +0500

    Update redirect.ps1

--- redirect.ps1
+++ redirect.ps1
@@ -1,11 +1,11 @@
 
 @{
     # "Команда" = "Имя_файла_на_гитхабе"
-    "physics"  = "WindowDance-physics.ps1"
-    "circle"   = "WindowDance-circle.ps1"
-    "sinus"    = "WindowDance-sinus.ps1"
-    "caps"     = "WindowDance-caps.ps1"
-    "chaos"    = "WindowDance-chaos.ps1"
+    "physics"  = "Window/WindowDance-physics.ps1"
+    "circle"   = "Window/WindowDance-circle.ps1"
+    "sinus"    = "Window/WindowDance-sinus.ps1"
+    "caps"     = "Window/WindowDance-caps.ps1"
+    "chaos"    = "Window/WindowDance-chaos.ps1"
     "debug"    = "Debug.ps1"
     "manager"  = "Menu.ps1"
     "download" = "Downloader.ps1"
@@ -13,8 +13,8 @@
     # Алиасы (синонимы)
     "local"    = "Downloader.ps1"
     "menu"     = "Menu.ps1"
-    "shake"    = "WindowDance-chaos.ps1"
-    "jump"     = "WindowDance-sinus.ps1"
-    "dance"    = "WindowDance-circle.ps1"
+    "shake"    = "Window/WindowDance-chaos.ps1"
+    "jump"     = "Window/WindowDance-sinus.ps1"
+    "dance"    = "Window/WindowDance-circle.ps1"
     "stop"     = "kill"
 }
